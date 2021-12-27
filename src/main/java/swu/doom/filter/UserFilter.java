8b36ca8a324724715390d96713b0e9a2f1ce9a38
Guru <1726736665@qq.com>
2021-12-27 17:15:10 +0800

    add a userfilter

---
+++ src/main/java/swu/doom/filter/UserFilter.java
@@ -0,0 +1,29 @@
+package swu.doom.filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class UserFilter extends HttpFilter {
+	
+	public final static String LOGIN_STATUS_USER = "LOGIN_STATUS_USER";
+	public final static String LOGIN_VALIDATE_CODE = "LOGIN_VALIDATE_CODE";	
+
+	public void doFilter(HttpServletRequest request,
+			HttpServletResponse response, FilterChain chain)
+			throws java.io.IOException, ServletException {
+
+		HttpSession session = request.getSession(true);//创建会话
+		Boolean status = (Boolean) session.getAttribute(LOGIN_STATUS_USER);//检查登录状态
+
+		if (status == null || status.equals(Boolean.FALSE)) {
+			response.sendRedirect("/VoteSystem/login.html");//重定向
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+}
